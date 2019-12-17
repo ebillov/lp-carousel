6059eb24a92a6fa13cb9f29e0a501f83f2042b2c
ebillov <ebillov@gmail.com>
2019-12-17 23:55:37 +0800

    Completed phase 1 editing.

--- lp-carousel.php
+++ lp-carousel.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Carousel LP
 * Plugin URI: https://virson.wordpress.com/
-* Description: A wordpress extension plugin to Listing Pro plugin for showing carousel sliders on selected listings. Read more the <a href="https://github.com/ebillov/lp-carousel" target="_blank">guide</a> on how to use this plugin.
+* Description: A wordpress extension plugin to Listing Pro plugin for showing carousel sliders on selected listings. Read the <a href="https://github.com/ebillov/lp-carousel">guide</a> on how to use this plugin.
 * Version: 0.0.1b
 * Author: Virson Ebillo
 * Author URI: https://virson.wordpress.com/
@@ -23,65 +23,83 @@ if(function_exists('is_plugin_active')){
 //Define our constants
 define('CAROUSEL_LP_DIR_URL', plugin_dir_url( __FILE__ ));
 
-/**
- * Begin printing our scripts
- */
-add_action('wp_head', function(){
-	
-	//Get the current post object
-	global $post;
-	
-	//Detect if the shortcode is placed in the content
-	if( has_shortcode( $post->post_content, 'carousel_lp' ) ){
-		wp_enqueue_script('jquery');
-		wp_enqueue_style(
-			'carousel-lp',
-			CAROUSEL_LP_DIR_URL . '/includes/slick/slick.css',
-			array(),
-			'0.0.1b'
-        );
-		wp_enqueue_style(
-			'carousel-lp-theme',
-			CAROUSEL_LP_DIR_URL . '/includes/slick/slick-theme.css',
-			array(),
-			'0.0.1b'
-        );
-        
-	}
-	
-});
-
-add_action('wp_footer', function(){
-
-	//Get the current post object
-	global $post;
-	
-	//Detect if the shortcode is placed in the content
-	if( has_shortcode( $post->post_content, 'carousel_lp' ) ){
-        wp_enqueue_script(
-            'carousel-lp-script',
-            CAROUSEL_LP_DIR_URL . '/includes/slick/slick.min.js',
-			array(),
-			'0.0.1b'
-        );
-    }
-    
-});
-
 //Our shortcode
 add_shortcode('carousel_lp', function($atts){
 
+    //Define defaults
+    $listing_ids = [];
+    $plan_ids = [];
+
 	//Define shortcode attributes (defaults)
     $atts = shortcode_atts( array(
-        'listing_ids' => [],
-        'plan_ids' => []
+        'listing_ids' => $listing_ids,
+        'plan_ids' => $plan_ids
     ), $atts );
+
+    //Get the attributes
+    $listing_ids = (!empty($atts['listing_ids'])) ? explode(',', $atts['listing_ids']) : $listing_ids;
+    $plan_ids = (!empty($atts['plan_ids'])) ? explode(',', $atts['plan_ids']) : $plan_ids;
+	
+	//Define invalid characters
+	$invalid_chars_attr = array( "@", "/", "!", "=", "+", "$", "%", "^", "&", "(", ")", ".", "#", "*", ":", ";", "'", '"', "|", "?", "{", "}", "[", "]", "`", "~", "<", ">", " " );
+	
+	//Replace invalid characters
+	$listing_ids = str_replace( $invalid_chars_attr, array(), $listing_ids );
+    $plan_ids = str_replace( $invalid_chars_attr, array(), $plan_ids );
+
+    //Begin get posts
+    $args = array(
+        'numberposts' => -1,
+        'orderby' => 'date',
+        'order' => 'rand',
+        'post_type' => 'listing',
+        'include' => $listing_ids,
+        'post_status' => 'publish',
+        'suppress_filters' => true,
+    );
+
+    //Get all the posts by the args
+    $listings = get_posts($args);
+
+    //Do this to filter the listings by plan ids
+    if(!empty($plan_ids)){
+
+        //Container for our posts
+        $filtered_ids = [];
+
+        //Loop through each listing
+        foreach($listings as $listing){
+
+            //Get the custom meta field
+            $custom_field = get_post_meta($listing->ID, 'lp_listingpro_options', true);
+
+            //Only get the posts with Plan_id values
+            if(!empty($custom_field['Plan_id'])){
+
+                //Check if the plan id is within the defined array
+                if(in_array($custom_field['Plan_id'], $plan_ids)){
+                    $filtered_ids[] = $listing;
+                }
+
+            }
+
+        }
+
+        //Redefine it
+        $listings = $filtered_ids;
+
+    }
+
+    //Check if it was empty
+    if(empty($listings)){
+        return '<p>No listings found.</p>';
+    }
 
     //Begin object buffer
     ob_start();
 
     //Include the default template
-    include('includes/front-end-script.php');
+    include('templates/default.php');
 
     //Get object buffer, clean and return it
     return ob_get_clean();
--- templates/default.php
+++ templates/default.php
@@ -1,3 +1,262 @@
-<div class="default-carousel-lp-template">
-    <p>test</p>
+<?php
+    global $listingpro_options;
+    $lp_review_switch = $listingpro_options['lp_review_switch'];
+?>
+<style>
+    #carousel-lp-shortcode {
+        display: none;
+        margin: 30px 0;
+    }
+    #carousel-lp-shortcode .slick-list {
+        margin: 0 -15px;
+    }
+    #carousel-lp-shortcode .slick-slide {
+        margin: 0 15px;
+    }
+    #carousel-lp-shortcode .slick-prev {
+        left: -46px;
+    }
+    #carousel-lp-shortcode .slick-prev:before,
+    #carousel-lp-shortcode .slick-next:before {
+        -webkit-box-shadow: 0 1px 14px 0 rgba(0, 0, 0, 0.3);
+        -moz-box-shadow: 0 1px 14px 0 rgba(0, 0, 0, 0.3);
+        box-shadow: 0 1px 14px 0 rgba(0, 0, 0, 0.3);
+    }
+    #carousel-lp-shortcode .carousel-item {
+        border: 1px solid #e3e3e3;
+        border-radius: 8px;
+        background: #ffffff;
+    }
+    #carousel-lp-shortcode .carousel-item .item-content {
+        padding: 10px 15px;
+        border-top: 1px solid #e3e3e3;
+    }
+    #carousel-lp-shortcode .carousel-item .item-content h4 {
+        font-weight: bold;
+    }
+    /*
+    #carousel-lp-shortcode .carousel-item .item-img {
+        position: relative;
+    }
+    #carousel-lp-shortcode .carousel-item .item-img .overlay-gradient {
+        background: rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(0, 0, 0, .9) 8%, rgba(0, 0, 0, 0) 94%) repeat scroll 0 0;
+        height: 50px;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+    }
+    */
+    #carousel-lp-shortcode .carousel-item .item-content ul {
+        border-top: 1px solid #dddddd;
+        margin-top: 10px;
+    }
+    #carousel-lp-shortcode .carousel-item .item-content ul li {
+        padding: 10px 0;
+        border-bottom: 1px solid #dddddd;
+    }
+    #carousel-lp-shortcode .carousel-item .item-content ul li:last-child {
+        border-bottom: none;
+    }
+    #carousel-lp-shortcode .carousel-item .item-content .address_section img {
+        width: 17px;
+        display: inline-block;
+    }
+</style>
+<div id="carousel-lp-shortcode" class="default-carousel-lp-template">
+
+    <?php foreach($listings as $listing): ?>
+    <div class="carousel-item">
+        <div class="item-img">
+            <?php
+                if ( has_post_thumbnail()) {
+                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $listing->ID ), 'listingpro-blog-grid2' );
+                    if(!empty($image[0])){
+                        echo '<a href="'. get_the_permalink($listing->ID) . '">
+                                <img src="' . $image[0] . '" />
+                            </a>';
+                    }else {
+                        echo '
+                        <a href="'. get_the_permalink($listing->ID) .'" >
+                            <img src="'.esc_html__('https://via.placeholder.com/372x400', 'listingpro').'" alt="">
+                        </a>';
+                    }	
+                } elseif(!empty( lp_default_featured_image_listing() )){
+                    echo '<a href="'. get_the_permalink($listing->ID) . '" >
+                        <img src="' . lp_default_featured_image_listing() . '" />
+                    </a>';
+                } else {
+                    echo '
+                    <a href="'. get_the_permalink($listing->ID) . '" >
+                        <img src="'.esc_html__('https://via.placeholder.com/372x400', 'listingpro').'" alt="">
+                    </a>';
+                }
+            ?>
+            <div class="overlay-gradient"></div>
+        </div>
+        <div class="item-content">
+            <h4><a href="<?php echo get_the_permalink($listing->ID); ?>"><?php echo $listing->post_title; ?></a></h4>
+            <?php
+                $a = get_post_meta($listing->ID, 'listing_plan_data');
+                $b = 'price';
+
+                    
+                if(empty($a)){
+                    $a = get_post_meta($listing->ID, 'lp_listingpro_options');
+                    $b = 'Plan_id';
+                    if(array_key_exists($b, $a[0])){
+                        if($a[0]['Plan_id'] == ''){
+                            $b = 'changed_planid';
+                            if(array_key_exists($b, $a[0])){
+                                if($a[0]['changed_planid'] == ''){
+                                    $b = 'lp_purchase_price';
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $color = '';
+                if(array_key_exists($b, $a[0])){
+                    if($b == 'changed_planid' || $b == 'Plan_id'){
+                        if($a[0][$b] == '1255' || $a[0][$b] == '211'){
+                            $color = '#9b111e';
+                        } elseif($a[0][$b] == '1256' || $a[0][$b] == '205'){
+                            $color = "#0f52ba";
+                        } elseif($a[0][$b] == '1257' || $a[0][$b] == '208'){
+                            $color = '#dd9933';
+                        } else {
+                            $color = 'gray';
+                        }
+                    }else{
+                        if($a[0][$b] == '448' || $a[0][$b] == '45'){
+                            $color = '#9b111e';
+                        } elseif($a[0][$b] == '77' || $a[0][$b] == '785'){
+                            $color = '#0f52ba';
+                        } elseif($a[0][$b] == '2244' || $a[0][$b] == '220'){
+                            $color = '#dd9933';
+                        } else {
+                            $color = 'gray';
+                        }
+                    }
+                } else {
+                    $color = 'gray';
+                }
+            ?>
+            <span style="color: <?php echo $color; ?>">
+            <?php
+                if(array_key_exists($b, $a[0])){
+                    if($b == 'changed_planid' || $b == 'Plan_id'){
+                        if($a[0][$b] == '1255' || $a[0][$b] == '211'){
+                            echo 'Ruby';
+                        } else if($a[0][$b] == '1256' || $a[0][$b] == '205'){
+                            echo 'Sapphire';
+                        } else if($a[0][$b] == '1257' || $a[0][$b] == '208'){
+                            echo 'Diamond';
+                        } else {
+                            echo 'Free';
+                        }
+                    } else {
+                        if($a[0][$b] == '448' || $a[0][$b] == '45'){
+                            echo 'Ruby';
+                        } else if($a[0][$b] == '77' || $a[0][$b] == '785'){
+                            echo 'Sapphire';
+                        } else if($a[0][$b] == '2244' || $a[0][$b] == '220'){
+                            echo 'Diamond';
+                        } else {
+                            echo 'Free';
+                        }
+                    }
+                } else {
+                    echo 'Free';
+                }
+            ?>
+            </span>
+            <ul>
+                <!--
+                <?php if($lp_review_switch ==1 ): ?>
+                <li>
+                    <?php
+                    $NumberRating = listingpro_ratings_numbers($listing->ID);
+                    if($NumberRating != 0):
+                        if($NumberRating <= 1){
+                            $review = esc_html__('Rating', 'listingpro');
+                        }else{
+                            $review = esc_html__('Ratings', 'listingpro');
+                        }
+                        echo lp_cal_listing_rate($listing->ID);
+                    ?>
+                    <span>
+                        <?php echo $NumberRating; ?>
+                        <?php echo $review; ?>
+                    </span>
+                    <?php else: echo lp_cal_listing_rate($listing->ID); endif; ?>
+                </li>
+                <?php endif; ?>
+                -->
+
+                <!-- <li class="middle tester">
+                    <?php //echo listingpro_price_dynesty_text($listing->ID); ?>
+                </li> -->
+                <li class="cattest">
+                    <?php
+                        $cats = get_the_terms( $listing->ID, 'listing-category' );
+                        
+                        if(!empty($cats)){
+                            $catCount = 1;
+                            foreach ( $cats as $cat ) {
+
+                                $category_image = listing_get_tax_meta($cat->term_id, 'category', 'image');
+                                if(!empty($category_image)){
+                                    echo '<span class="cat-icon"><img class="icon icons8-Food" src="' . $category_image . '" alt="cat-icon"></span>';
+                                }
+                                $term_link = get_term_link( $cat );
+                                echo '
+                                    <span class="cat_link" link="'. $term_link. '">
+                                        '.$cat->name.'
+                                    </span>
+                                ';
+                                if($catCount < count($cats)){
+                                    echo '| ';
+                                }
+                                $catCount++;
+
+                            }
+                        }
+                    ?>
+                </li>
+            </ul>
+            <?php
+                $countlocs = 1;
+                $cats = get_the_terms( $listing->ID, 'location' );
+                if(!empty($cats)){
+                    echo '<div class="address_section">';
+                    echo listingpro_icons('mapMarkerGrey');
+                    foreach ( $cats as $cat ) {
+                        if($countlocs == 1){
+                            $term_link = get_term_link( $cat );
+                            echo '
+                            <a href="' . $term_link . '">
+                                ' . $cat->name . '
+                            </a>';
+                        }
+                        $countlocs ++;
+                    }
+                    echo '</div>';
+                }
+            ?>
+        </div>
+    </div>
+    <?php endforeach; ?>
+
 </div>
+<script>
+jQuery(document).ready(function(){
+    jQuery('#carousel-lp-shortcode').show().slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
+</script>
